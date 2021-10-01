3f938d541c2ed49c5edefd52e61384eed1b9f3d7
ekomh170 <ekomh020@gmail.com>
2021-10-01 22:14:40 +0700

    memperbarui config/uri.php | memperbarui config buatan (uri.php)

--- config/uri.php
+++ config/uri.php
@@ -2,6 +2,6 @@
 
 function base_url()
 {
-    $base_url = "http://localhost:8080/github/app-informasi-keluar-masuk-surat/"; // disesuaikan sesuai direktori root dokumen
+    $base_url = "http://localhost:8080/github/aplikasi_informasi_keluar_masuk_surat_sederhana/"; // disesuaikan sesuai direktori root dokumen
     return $base_url;
 }
